eebee897e2ac5653d0123e54a3077d1bd03ca61e
Aadarsh <aadarsh.ram@gmail.com>
2019-12-07 13:35:15 +0530

    Added video

--- mac.py
+++ mac.py
@@ -2,7 +2,7 @@ import nmap
 #First, you need a Postscanner object that will be used to do the scan
 nm = nmap.PortScanner()
 #You can then do a scan of all the IPV4 addresses provided by the network you are connected to
-nm.scan(hosts = '192.168.2.1/24', arguments = '-sn')
+nm.scan(hosts = '192.168.2.*', arguments = '-sn')
 for host in nm.all_hosts():
 	#If the status of an IP address is not down, print it
 	if nm[host]['status']['state'] != "down":
